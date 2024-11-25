1656510e449599b0e784f3cf46031f7be72fbfb6
hokke <minoridaikubara@gmail.com>
2024-11-25 21:16:06 +0900

    コメントを追加

--- Models/ConnectionInfoModel.cs
+++ Models/ConnectionInfoModel.cs
@@ -1,52 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Configuration;
 
 namespace WpfApp5.Models
 {
 	internal class ConnectionInfoModel
 	{
+		#region フィールドプロパティ
+		/// <summary>
+		/// ホスト
+		/// </summary>
 		public string Host { get; }
-		public string User { get; }
-		public string Database { get; }
 
+		/// <summary>
+		/// ユーザー名
+		/// </summary>
+		public string User { get; }
+
+		/// <summary>
+		/// データベース
+		/// </summary>
+		public string Database { get; }
+		#endregion
+
+		#region コンストラクタ
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
 		public ConnectionInfoModel()
 		{
-			Host = GetApp("host");
-			User = GetApp("user");
-			Database = GetApp("database");
+			Host = GetAppCofing("host");
+			User = GetAppCofing("user");
+			Database = GetAppCofing("database");
 		}
 
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		/// <param name="host">ホスト</param>
+		/// <param name="user">ユーザー名</param>
+		/// <param name="database">データベース</param>
 		public ConnectionInfoModel(string host, string user, string database)
 		{
 			Host = host;
 			User = user;
 			Database = database;
 		}
+		#endregion
 
-		private string GetApp(string key)
+		#region publicメソッド
+		/// <summary>
+		/// 接続情報を取得する
+		/// </summary>
+		/// <returns>接続情報</returns>
+		public string ConnectionInfo()
+		{
+			return $"Host={Host};Username={User};Database={Database}";
+		}
+
+		/// <summary>
+		/// アプリ設定を保存
+		/// </summary>
+		public void Save()
+		{
+			var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+			SetAppConfig(config, "host", Host);
+			SetAppConfig(config, "user", User);
+			SetAppConfig(config, "database", Database);
+			config.Save(ConfigurationSaveMode.Modified);
+			ConfigurationManager.RefreshSection(config.AppSettings.SectionInformation.Name);
+		}
+		#endregion
+
+		#region privateメソッド
+		/// <summary>
+		/// アプリ設定を取得
+		/// </summary>
+		/// <param name="key">キー</param>
+		/// <returns>値</returns>
+		private string GetAppCofing(string key)
 		{
 			string? value = ConfigurationManager.AppSettings[key];
 			if (value == null) return "";
 			else return value.ToString();
 		}
 
-		public string ConnectionInfo()
+		/// <summary>
+		/// アプリ設定をセット
+		/// </summary>
+		/// <param name="config">設定</param>
+		/// <param name="key">キー</param>
+		/// <param name="value">値</param>
+		private void SetAppConfig(Configuration config, string key, string value)
 		{
-			return $"Host={Host};Username={User};Database={Database}";
+			config.AppSettings.Settings[key].Value = value;
 		}
-
-		public void Save()
-		{
-			var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-			config.AppSettings.Settings["host"].Value = Host;
-			config.AppSettings.Settings["user"].Value = User;
-			config.AppSettings.Settings["database"].Value = Database;
-			config.Save(ConfigurationSaveMode.Modified);
-			ConfigurationManager.RefreshSection(config.AppSettings.SectionInformation.Name);
-		}
+		#endregion
 	}
 }
--- ViewModels/ConfigViewModel.cs
+++ ViewModels/ConfigViewModel.cs
@@ -7,16 +7,31 @@ namespace WpfApp5.ViewModels
 	class ConfigViewModel : ObservableObject
 	{
 		#region フィールドプロパティ
+		/// <summary>
+		/// ホスト
+		/// </summary>
 		public ReactivePropertySlim<string> Host { get; } = new ReactivePropertySlim<string>();
 
+		/// <summary>
+		/// ユーザー名
+		/// </summary>
         public ReactivePropertySlim<string> User { get; } = new ReactivePropertySlim<string>();
 
+		/// <summary>
+		/// データベース
+		/// </summary>
         public ReactivePropertySlim<string> Database { get; } = new ReactivePropertySlim<string>();
 
+		/// <summary>
+		/// 登録ボタン
+		/// </summary>
         public IRelayCommand CommandUpdate { get; }
 		#endregion
 
 		#region コンストラクタ
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
 		public ConfigViewModel()
         {
             var info = new ConnectionInfoModel();
@@ -30,6 +45,9 @@ namespace WpfApp5.ViewModels
 		#endregion
 
 		#region privateメソッド
+		/// <summary>
+		/// 更新ボタン
+		/// </summary>
 		private void OnCommandUpdate()
 		{
 			var info = new ConnectionInfoModel(Host.Value, User.Value, Database.Value);

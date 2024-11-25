11a5468244d2ff12e0cf134464e3c0409f79efab
hokke <minoridaikubara@gmail.com>
2024-11-25 19:06:16 +0900

    ・接続情報を保持するモデルを作成 ・設定画面とViewモデルを用意

---
+++ Models/ConnectionInfoModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp5.Models
+{
+	internal class ConnectionInfoModel
+	{
+		public string Host { get; }
+		public string User { get; }
+		public string Database { get; }
+
+		public ConnectionInfoModel()
+		{
+			Host = GetApp("host");
+			User = GetApp("user");
+			Database = GetApp("database");
+		}
+
+		private string GetApp(string key)
+		{
+			string? value = ConfigurationManager.AppSettings[key];
+			if (value == null) return "";
+			else return value.ToString();
+		}
+
+		public string ConnectionInfo()
+		{
+			return $"Host={Host};Username={User};Database={Database}";
+		}
+	}
+}
--- ViewModels/ConfigViewModel.cs
+++ ViewModels/ConfigViewModel.cs
@@ -1,13 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WpfApp5.UserControls;
-
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Reactive.Bindings;
+using WpfApp5.Models;
 namespace WpfApp5.ViewModels
 {
-    class ConfigViewModel
-    {
+	class ConfigViewModel : ObservableObject
+	{
+        public ReactivePropertySlim<string> Host { get; } = new ReactivePropertySlim<string>();
+
+        public ReactivePropertySlim<string> User { get; } = new ReactivePropertySlim<string>();
+
+        public ReactivePropertySlim<string> Database { get; } = new ReactivePropertySlim<string>();
+
+        public ConfigViewModel()
+        {
+            var info = new ConnectionInfoModel();
+
+            Host.Value = info.Host;
+            User.Value = info.User;
+            Database.Value = info.Database;
+        }
     }
 }
